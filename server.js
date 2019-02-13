6ea703a08d6925b0198a009a860fb6e3f1b56544
Cassio M. M. Pereira <cassiomartini@gmail.com>
2019-02-13 20:20:54 -0200

    user model and registration route

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
